3a9095b25f7ba7bc74e04c403152a947d037d1dc
peter max <petermax2004@gmail.com>
2019-06-15 14:08:01 +0300

    Add the init method to the credentials.py

--- credentials.py
+++ credentials.py
@@ -5,4 +5,18 @@ class Credentials:
 
     credentials_list = []
 
+    def __init__(self,password,credentials_name,credentials_password):
+        """
+        __init__ method defines the properties of credentials object
+        
+        Args:
+            password: Password of the user logged in.
+            credentials_name: Name of the account. 
+            credentials_password: Password for the account.
+        """
+        self.password = password
+        self.credentials_name = credentials_name
+        self.credentials_password = credentials_password
+
+
     
